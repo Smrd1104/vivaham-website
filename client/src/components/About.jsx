e348f1ac4b03214cdeaf97f42dc31b5d379eed6a
Smrd1104 <mriyasdeen1104@gmail.com>
2025-05-29 13:45:20 +0530

    changes

---
+++ client/src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1 className='text-[#F6A634] text-[1.5rem] text-center font-semibold'>About Us</h1>
+
+            <div>
+                <h2 className='text-[#2A2A2A] text-[3rem] text-center font-semibold'>Luxury, Tradition And Innovation - All in One Grand Experience</h2>
+                <p className='text-[#2A2A2A] text-[1rem] text-center font-semibold'>Vivaham Luxury Expo Pvt Ltd is not just a name, it’s a movement that redefines the grandeur of Indian weddings. We go beyond mere planning and bring together innovation, culture, and luxury under one roof. Whether it’s crafting dreamlike bridal experiences, curating industry-defining conventions, or connecting the finest brands with their perfect audience, Vivaham is where ideas turn into legacies. For us, every wedding is a story, and we ensure it’s told with unparalleled finesse.</p>
+            </div>
+        </div>
+    )
+}
+
+export default About
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import bgImage from '../assets/hero/bg-img.jpg'; // adjust path as needed
+import About from '../components/About';
 
 
 const Home = () => {
     return (
-        <div
-            className="min-h-screen bg-cover bg-center bg-no-repeat"
-            style={{ backgroundImage: `url(${bgImage})` }}
-        >
-            <Hero />
-            
+        <div className="relative min-h-screen">
+            {/* Background image layer */}
+            <div
+                className="absolute inset-0 bg-cover bg-center bg-no-repeat opacity-10 z-0"
+                style={{ backgroundImage: `url(${bgImage})` }}
+            ></div>
+
+
+            {/* Content layer */}
+            <div className="relative z-10">
+                <Home />
+                <About />
+            </div>
 
         </div>
+
     )
 }
 
